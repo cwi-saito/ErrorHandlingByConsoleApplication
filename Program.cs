373fd50d9a78ae7a717316e033e551bba4f77733
cwi-saito <saito@contentsworks.co.jp>
2017-08-04 14:32:24 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandlingByConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Register error handler
+            System.Threading.Thread.GetDomain().UnhandledException += new UnhandledExceptionEventHandler(Program_UnhandledException);
+
+            throw new Exception("Error");
+        }
+
+        public static void Program_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            // Write StackTrace and errors
+        }
+    }
+}
